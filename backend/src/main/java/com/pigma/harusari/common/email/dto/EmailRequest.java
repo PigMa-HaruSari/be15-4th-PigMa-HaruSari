caf16a7376b31471d4fc49a7923e7bb5323ab6d6
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-18 23:48:12 +0900

    feat: 이메일 인증코드 발송 및 검증 기능 구현 (#13)

---
+++ backend/src/main/java/com/pigma/harusari/common/email/dto/EmailRequest.java
@@ -0,0 +1,17 @@
+package com.pigma.harusari.common.email.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailRequest {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    private String code; // 인증 코드
+}
