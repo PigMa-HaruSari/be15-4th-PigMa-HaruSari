31015ec87d4e6efc5e6164ccd92f9e6ca901d6d9
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-18 23:40:11 +0900

    feat: 회원 기능을 위한 Member 엔티티 및 Gender enum 추가 (#13)

---
+++ backend/src/main/java/com/pigma/harusari/user/command/entity/Gender.java
@@ -0,0 +1,34 @@
+package com.pigma.harusari.user.command.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum Gender {
+    NONE(0),
+    MALE(1),
+    FEMALE(2);
+
+    private final int code;
+
+    Gender(int code) {
+        this.code = code;
+    }
+
+    public static Gender fromCode(int code) {
+        return switch (code) {
+            case 1 -> MALE;
+            case 2 -> FEMALE;
+            default -> NONE;
+        };
+    }
+
+    public static Gender fromString(String input) {
+        return switch (input.toUpperCase()) {
+            case "MALE" -> MALE;
+            case "FEMALE" -> FEMALE;
+            default -> NONE;
+        };
+    }
+
+
+}
---
+++ backend/src/main/java/com/pigma/harusari/user/command/entity/Member.java
@@ -0,0 +1,51 @@
+package com.pigma.harusari.user.command.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Enumerated(EnumType.ORDINAL) // DB에는 0, 1, 2 순서를 tinyint로 저장
+    @Column(name = "gender", nullable = false)
+    private Gender gender;
+
+    @Column(name = "user_registered_at", nullable = false)
+    private LocalDateTime userRegisteredAt;
+
+    @Column(name = "user_modified_at")
+    private LocalDateTime userModifiedAt;
+
+    @Column(name = "user_deleted_at")
+    private Boolean userDeletedAt;
+
+    @Builder
+    public Member(String email, String password, String nickname, Gender gender, LocalDateTime userRegisteredAt) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.gender = gender;
+        this.userRegisteredAt = userRegisteredAt;
+        this.userDeletedAt = false;
+    }
+}
