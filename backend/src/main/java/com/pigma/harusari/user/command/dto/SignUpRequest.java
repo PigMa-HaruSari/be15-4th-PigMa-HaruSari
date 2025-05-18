0b722ce89a9e4bc14737e6c731ea5719336cb888
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-19 01:36:38 +0900

    feat: 테스트 코드를 위해 @Builder 어노테이션 추가 (#13)

--- backend/src/main/java/com/pigma/harusari/user/command/dto/SignUpRequest.java
+++ backend/src/main/java/com/pigma/harusari/user/command/dto/SignUpRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@Builder
 public class SignUpRequest {
 
     @Email(message = "올바른 이메일 형식이 아닙니다.")
