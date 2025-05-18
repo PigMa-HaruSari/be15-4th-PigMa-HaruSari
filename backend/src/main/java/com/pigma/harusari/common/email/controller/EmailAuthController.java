389cad6d89db28f84d020c8bac02bc239e93c272
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-18 23:54:44 +0900

    chore: 이메일 인증 API 라우팅 경로에 버전 정보 추가 (#13)

--- backend/src/main/java/com/pigma/harusari/common/email/controller/EmailAuthController.java
+++ backend/src/main/java/com/pigma/harusari/common/email/controller/EmailAuthController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth/email")
+@RequestMapping("/api/v1/auth/email")
 @RequiredArgsConstructor
 public class EmailAuthController {
 
