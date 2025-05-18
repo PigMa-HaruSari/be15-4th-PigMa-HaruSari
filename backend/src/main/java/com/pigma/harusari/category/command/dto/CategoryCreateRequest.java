7e72a6e338a58db36279b3a7d87e3277299c39d2
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-18 23:43:24 +0900

    feat: 회원가입 시 사용할 카테고리 등록 로직 추가 (#13)

---
+++ backend/src/main/java/com/pigma/harusari/category/command/dto/CategoryCreateRequest.java
@@ -0,0 +1,15 @@
+package com.pigma.harusari.category.command.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryCreateRequest {
+    /* 회원이 최초 회원가입할 때 사용하는 dto */
+    @NotBlank(message = "카테고리 이름은 필수입니다.")
+    private String categoryName;
+
+    private String color; // 선택 사항 (nullable)
+}
