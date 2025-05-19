11a93826c0ce4b361a2ce6b974844f0cff153325
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-19 19:32:55 +0900

    chore: 테스트용 객체 생성을 위한 LoginRequest 생성자 및 빌더 추가 (#13)

--- backend/src/main/java/com/pigma/harusari/common/auth/dto/LoginRequest.java
+++ backend/src/main/java/com/pigma/harusari/common/auth/dto/LoginRequest.java
@@ -2,11 +2,15 @@ package com.pigma.harusari.common.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoginRequest {
 
     @Email(message = "올바른 이메일 형식이 아닙니다.")
