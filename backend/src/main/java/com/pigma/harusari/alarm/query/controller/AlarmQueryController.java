0c4f590e1743bfa0a23ab8af565442aa097c2220
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-20 17:44:01 +0900

    refactor: User 또는 UserDetails 호출을 CustomUserDetails 호출로 변경 (#44)

--- backend/src/main/java/com/pigma/harusari/alarm/query/controller/AlarmQueryController.java
+++ backend/src/main/java/com/pigma/harusari/alarm/query/controller/AlarmQueryController.java
@@ -2,11 +2,11 @@ package com.pigma.harusari.alarm.query.controller;
 
 import com.pigma.harusari.alarm.query.dto.AlarmResponseDto;
 import com.pigma.harusari.alarm.query.service.AlarmQueryService;
+import com.pigma.harusari.common.auth.model.CustomUserDetails;
 import com.pigma.harusari.common.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +21,9 @@ public class AlarmQueryController {
     private final AlarmQueryService alarmQueryService;
 
     @GetMapping("/unread")
-    public ResponseEntity<ApiResponse<List<AlarmResponseDto>>> getUnreadAlarms(@AuthenticationPrincipal User userDetails) {
+    public ResponseEntity<ApiResponse<List<AlarmResponseDto>>> getUnreadAlarms(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
         Long memberId = Long.parseLong(userDetails.getUsername());
 
         List<AlarmResponseDto> alarms = alarmQueryService.getUnreadAlarms(memberId);
