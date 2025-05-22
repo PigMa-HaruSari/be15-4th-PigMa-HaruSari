205bec8a8b7ebf5ec2c39011be54e850893b61a7
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-22 19:39:26 +0900

    style: 주석으로 동작 흐름 설명 추가 (#77)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,30 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { useUserStore } from "@/stores/userStore.js";
+
 import { statisticsRoutes } from '@/features/statistics/router.js';
 import { mainRoutes } from '@/features/main/router.js';
-import {userRoutes} from "@/features/user/router.js";
-import {useUserStore} from "@/stores/userStore.js";
-import {categoryRoutes} from "@/features/category/router.js";
+import { userRoutes } from "@/features/user/router.js";
+import { categoryRoutes } from "@/features/category/router.js";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [...statisticsRoutes, ...mainRoutes, ...userRoutes, ...categoryRoutes],
+  routes: [
+    ...statisticsRoutes,
+    ...mainRoutes,
+    ...userRoutes,
+    ...categoryRoutes
+  ],
 });
 
+// 비로그인 상태로 접근 시 차단하는 설정
 router.beforeEach((to) => {
   const userStore = useUserStore();
   const isLoggedIn = !!userStore.userId;
 
+  // 비회원도 접속 가능한 화면
   const publicPages = ['/login', '/signup', '/reset-password'];
   const authRequired = !publicPages.includes(to.path);
 
+  // 회원만 접속 가능한 페이지이나 비로그인 상태 : 로그인 페이지로 이동
   if (authRequired && !isLoggedIn) {
     return { name: 'login', query: { redirect: to.fullPath } };
   }
 
+  // 이미 로그인 된 상황에서 로그인 페이지에 접근하면 메인으로 이동
   if (to.name === 'login' && isLoggedIn) {
     return { name: 'main' };
   }
 });
-
 
 export default router;
--- frontend/src/stores/userStore.js
+++ frontend/src/stores/userStore.js
@@ -1,25 +1,30 @@
+/* Pinia 설정 */
 import { defineStore } from 'pinia';
 
-/* Pinia 사용자 정보 저장소 */
-let logoutTimer = null; // ⏱️ 타이머 전역 변수
+// 자동 로그아웃 기능을 위한 타이머 변수
+let logoutTimer = null;
 
+/* 'user'라는 이름의 Store 정의 */
 export const useUserStore = defineStore('user', {
+    // 로그인 한 유저 정보
     state: () => ({
         userId: null,
         email: '',
         nickname: ''
     }),
+
+    // 상태 변경 메소드 (로그인 성공, 로그아웃)
     actions: {
+        // 로그인 성공 시 사용자 정보를 localStorage에 저장
         setUser(userData) {
             this.userId = userData.userId;
             this.email = userData.email;
             this.nickname = userData.nickname;
 
-            // ✅ localStorage 저장
             localStorage.setItem('user', JSON.stringify(userData));
             localStorage.setItem('accessToken', userData.accessToken);
-
         },
+        // 로그아웃 시 localStorage에서 사용자 정보를 삭제
         logout() {
             this.userId = null;
             this.email = '';
@@ -29,9 +34,7 @@ export const useUserStore = defineStore('user', {
             localStorage.removeItem('accessToken');
             localStorage.removeItem('refreshToken');
 
-
-            clearTimeout(logoutTimer);
+            clearTimeout(logoutTimer); // TODO : 타이머 추가하기
         }
-
     }
 });
