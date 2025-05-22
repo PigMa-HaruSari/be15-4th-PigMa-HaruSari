2d1b631c8746599d16761fc051e5f6d27430e129
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-22 19:58:19 +0900

    style: main.js 코드 정렬 및 시맨틱 구간 분리 (#77)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,14 @@
 import {createApp} from 'vue'
 import {createPinia} from 'pinia'
+
 import '/src/assets/css/header.css'
 import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
 
-
 import App from './App.vue'
 import router from './router'
 
+import { useUserStore } from "@/stores/userStore.js";
 import {
     Chart,
     ArcElement,
@@ -19,24 +20,21 @@ import {
     DoughnutController,
     BarController
 } from 'chart.js'
-import {useUserStore} from "@/stores/userStore.js";
 
-Chart.register(
-    ArcElement,
-    BarElement,
-    CategoryScale,
-    LinearScale,
-    Tooltip,
-    Legend,
-    DoughnutController,
-    BarController
-)
-
+// 앱 기본 전역 플로그인 설정
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+// localStorage 관련 설정
+const userStore = useUserStore();
+const savedUser = localStorage.getItem('user');
+if (savedUser) {
+    userStore.setUser(JSON.parse(savedUser));
+}
+
+// Toast 설정
 app.use(Toast, {
     position: 'top-right',
     timeout: 3000,
@@ -52,12 +50,16 @@ app.use(Toast, {
     rtl: false,
 });
 
-const userStore = useUserStore();
-const savedUser = localStorage.getItem('user');
-if (savedUser) {
-    userStore.setUser(JSON.parse(savedUser));
-}
-
-
+// 차트 관련 설정
+Chart.register(
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+  DoughnutController,
+  BarController
+)
 
 app.mount('#app')
