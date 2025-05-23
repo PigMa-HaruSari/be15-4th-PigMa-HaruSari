88dd789dd0d2689f24840b8212c726a732343fad
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-23 11:54:46 +0900

    refactor: 회원가입 관련 API 엔드포인트 meta 수정 (#87)

--- frontend/src/features/user/api.js
+++ frontend/src/features/user/api.js
@@ -7,15 +7,15 @@ export function loginUser(data) {
 export function sendEmailCode(email) {
   console.log("*** sendEmailCode 실행 ***")
   console.log(email)
-  return api.post('/api/v1/auth/email/send', { email });
+  return api.post('/auth/email/send', { email });
 }
 
 export function verifyEmailCode(email, code) {
-  return api.post('/api/v1/auth/email/verify', {
+  return api.post('/auth/email/verify', {
     email, code
   });
 }
 
 export function registerUser(payload) {
-  return api.post('/api/v1/auth/signup', payload);
+  return api.post('/auth/signup', payload);
 }
